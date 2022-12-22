c71519d8beb777f74fd1b35b933a3d34a8ddf6ae
Huore95 <937832539@qq.com>
2022-12-22 22:23:12 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+
+
+let app = createApp(App);
+
+//引入字体图标的样式
+import "./assets/fonts/iconfont.css"
+
+//引入 flexible 适配
+import "./assets/flexible.js"
+
+//引入重置样式
+import "./assets/reset.css"
+
+//引入组件样式
+import vant from 'vant';
+import 'vant/lib/index.css';
+app.use(vant)
+
+//注册路由
+app.use(router)
+app.mount('#app')
